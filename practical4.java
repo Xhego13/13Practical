de4e958d8134f3e717a20127038013293639aa6e
Mzawupheli Xhego <4428828@myuwc.ac.za>
2026-02-23 15:39:09 +0200

    Add linear and binary search methods

--- practical4.java
+++ practical4.java
@@ -1,1 +1,21 @@
-
+  static void oneofyourMethods(int n,int[] anyArr,int target){
+      static int linearsearch(int[] anyArr, int target){
+         for(int i=0;i<=anyArr.length-1;i++){
+            if(anyArr[i]==target){return target;}
+         }
+         return -1;
+      }
+      static int binarysearch(int[] anyArr,int target){
+         int end= anyArr.length-1;
+         int start;
+         int midNum= (start+end)/2;
+         while(start<=end){
+            if(anyArr[midNum]==target){return midNum;}
+            else if (anyArr[midNum]<target){start=midNum+1;}
+            else{end=midNum-1;} 
+            }
+      }
+         return-1;
+      }
+   }
+   }
